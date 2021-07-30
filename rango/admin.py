c647c5707edbac02b5603ffc470ad4dcb0000ea0
Yuan Fang <2532525F@student.gla.ac.uk>
2021-07-30 10:30:16 +0100

    6.3 Creating a Details Page

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,9 @@ from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
-    
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+       
 admin.site.register(Category)
 admin.site.register(Page,PageAdmin)
 # Register your models here.
