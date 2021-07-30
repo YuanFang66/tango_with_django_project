3fc5b0285784be60750937fbca45f32bc2d09465
Yuan Fang <2532525F@student.gla.ac.uk>
2021-07-30 13:52:29 +0100

    7.5Cleaner Forms

--- rango/forms.py
+++ rango/forms.py
@@ -16,6 +16,18 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the pages.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
+
+
     class Meta:
         model = Page
         exclude = ('category',) 
