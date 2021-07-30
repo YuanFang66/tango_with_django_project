cdeb6c49e665b188d8f81d137f2ff0134b83bcdf
Yuan Fang <2532525F@student.gla.ac.uk>
2021-07-30 16:34:16 +0100

    10 exercise

--- rango/views.py
+++ rango/views.py
@@ -18,14 +18,16 @@ def index(request):
     context_dict['extra'] = 'From the model solution on GitHub'
     
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
 
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    
-    return render(request, 'rango/about.html')
+   
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
